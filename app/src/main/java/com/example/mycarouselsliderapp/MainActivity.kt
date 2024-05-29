84637edb51a5f62a091cd650256e2023928ac6a7
Fajar baihaqi <muhammadfajarbaihaqi27@gmail.com>
2024-05-29 17:19:49 +0700

    Membuat Aplikasi Carousel Image Slider Menggunakan ViewPager2 (v1)

---
+++ app/src/main/java/com/example/mycarouselsliderapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.mycarouselsliderapp
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  viewPager2: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var imageList:ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+        setUpTransformer()
+    }
+
+    private fun setUpTransformer(){
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init(){
+        viewPager2 = findViewById(R.id.viewPager2)
+        handler = Handler(Looper.getMainLooper())
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.one)
+        imageList.add(R.drawable.two)
+        imageList.add(R.drawable.three)
+        imageList.add(R.drawable.four)
+        imageList.add(R.drawable.five)
+
+        adapter = ImageAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+    }
+}
